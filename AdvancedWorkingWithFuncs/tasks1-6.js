2a26426339b44a40461351f0d4c79adf4c205de9
canzoroglu <canzoroglu@gmail.com>
2019-02-08 12:44:55 +0300

    Add delaying decorator

--- AdvancedWorkingWithFuncs/tasks1-6.js
+++ AdvancedWorkingWithFuncs/tasks1-6.js
@@ -2,7 +2,7 @@
 
 
 
-//Spy decorator
+//Spy Decorator
 
 //returns a wrapper function which has a property named "calls"
 //calls is an array which includes arguments of past invations
@@ -15,3 +15,25 @@ function spy(fn){
   }
   return wrapper;
 }
+
+
+//Delaying Decorator
+
+//takes a func and a delay, returns a function which will be delayed with the given amount of time when it is run
+function f(x) {
+  console.log(this.a);
+  console.log(x);
+}
+
+function delay(fn, ms){
+  return function(){
+    setTimeout(() => fn.apply(this, arguments), ms)
+  };
+}
+// tests for the delaying func
+let obj1 = {a: 2};
+var a = 3;
+let f1000 = delay(f, 1000);
+let f1500 = delay(f, 1500);
+f1000.call(obj1, "test1"); // shows "test" after 1000ms
+f1500("test2"); // shows "test" after 1500ms
